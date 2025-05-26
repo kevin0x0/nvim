0f9857a62985e2c3586c6f9ca078d97e2b6935c1
kevin <kevin0x00@163.com>
2025-05-26 19:08:55 +0800

    don't automatically scroll

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -18,12 +18,12 @@ return {
     insert_mappings = true,     -- whether or not the open mapping applies in insert mode
     terminal_mappings = true,   -- whether or not the open mapping applies in the opened terminals
     persist_size = true,
-    persist_mode = false,       -- if set to true (default) the previous terminal mode will be remembered
+    persist_mode = true,       -- if set to true (default) the previous terminal mode will be remembered
     direction = 'float',
     close_on_exit = true,       -- close the terminal window when the process exits
     -- Change the default shell. Can be a string or a function returning a string
     shell = vim.o.shell,
-    auto_scroll = true,   -- automatically scroll to the bottom on terminal output
+    auto_scroll = false,   -- automatically scroll to the bottom on terminal output
     -- This field is only relevant if direction is set to 'float'
     float_opts = {
       -- The border key is almost the same as 'nvim_open_win'
