5294ce633d72e68079ede30f5aec5fbed5039fd2
kevin <kevin0x00@163.com>
2025-07-16 22:41:16 +0800

    don't ignore any file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,11 @@ vim.keymap.set('n', '<Leader>fb', function()
   require "telescope.builtin".current_buffer_fuzzy_find()
 end, opt)
 vim.keymap.set('n', '<Leader>ff', function()
-  require "telescope.builtin".find_files()
+  require "telescope.builtin".find_files {
+    hidden = true,
+    no_ignore = true,
+    no_ignore_parent = true,
+  }
 end, opt)
 vim.keymap.set('n', '<Leader>fg', function()
   require "telescope.builtin".live_grep()
