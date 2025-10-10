40a237e33e4782fb0203d016773c13be649ac520
kevin <kevin0x00@163.com>
2025-10-10 14:18:43 +0800

    format: add missing space

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,8 +8,8 @@ return {
     options = {
       icons_enabled = true,
       theme = "auto",
-      section_separators = { left = "", right ="" },
-      component_separators = { left = "", right ="" },
+      section_separators = { left = "", right = "" },
+      component_separators = { left = "", right = "" },
       globalstatus = true,
       always_divided_middle = false
     },
