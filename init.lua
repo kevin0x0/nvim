aefca7f1195f257000a068d013739f55bbad6d81
kevin <kevin0x00@163.com>
2026-01-07 00:14:04 +0800

    correctly set colorscheme in various os

--- init.lua
+++ init.lua
@@ -12,7 +12,20 @@ require "common.lsp"
 
 local colorscheme = os.getenv("NVIM_COLORSCHEME")
 if not colorscheme then
-  colorscheme = os.getenv('TERM') ~= 'linux' and 'tokyonight-night' or 'evening'
+  local function has_display()
+    local osname = vim.uv.os_uname().sysname;
+    if osname == 'Windows' or osname == 'Darwin' then
+      return true
+    end
+    if os.getenv("DISPLAY") then
+      return true
+    end
+    if os.getenv("WAYLAND_DISPLAY") then
+      return true
+    end
+    return false
+  end
+  colorscheme = has_display() and 'tokyonight-night' or 'elflord'
 end
 
 vim.cmd.colorscheme(colorscheme)
